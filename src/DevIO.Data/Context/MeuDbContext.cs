cb360fb0330f38e67a46afafabfcab05d8fdc2d6
Ulisses Duo <duoulisses@gmail.com>
2023-08-06 10:52:32 -0300

    first commit

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,36 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Addresss { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            //bisable cascade deletion
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+            
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
